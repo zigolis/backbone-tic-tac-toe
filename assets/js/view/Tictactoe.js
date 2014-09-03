e0e04b7f5b075105fd669c46ada9ff54b2e4b802
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2014-09-03 02:50:14 -0300

    Improved match method and added UI controlers to play again and restart game

--- assets/js/view/Tictactoe.js
+++ assets/js/view/Tictactoe.js
@@ -1,127 +1,83 @@
 var app = app || {};
 
 app.TictactoeView = Backbone.View.extend({
+    counter: 1,
+    playerX: 'X',
+    playerO: '0',
+
     el: '.stage',
 
     events: {
-        'click li'     : 'isEmpty',
-        'click button' : 'restartGame'
-    },
-
-    initialize: function(){
-        player = 1;
-    },
-
-    restartGame: function() {
-        player = 1;
-        this.$el.find('li').html('');
-    },
-
-    getValuesMult: function() {
-        map = [
-            [
-                this.$el.find('li').get(0).innerHTML,
-                this.$el.find('li').get(1).innerHTML,
-                this.$el.find('li').get(2).innerHTML
-            ],
-            [
-                this.$el.find('li').get(3).innerHTML,
-                this.$el.find('li').get(4).innerHTML,
-                this.$el.find('li').get(5).innerHTML
-            ],
-            [
-                this.$el.find('li').get(6).innerHTML,
-                this.$el.find('li').get(7).innerHTML,
-                this.$el.find('li').get(8).innerHTML
-            ]
-        ];
-    },
-
-    checkWinner: function() {
-        this.getValuesMult();
-
-        combinations = [
-                            [0,1,2],[3,4,5],[6,7,8],
-                            [0,3,6],[1,4,7],[2,5,8],
-                            [0,4,8],[2,4,6]
-                       ];
-
-        for (var a = 0; a < combinations.length; a++) {
-            console.log(combinations[1]);
-            if( combinations[a][0] == 'X' && combinations[a][1] == 'X' && combinations[a][2] == 'X'){
-                console.log(combinations[a][0]);
-            }
-        };
-    },
-
-    getValues: function() {
-        map = [
-            this.$el.find('li').get(0).innerHTML,
-            this.$el.find('li').get(1).innerHTML,
-            this.$el.find('li').get(2).innerHTML,
-            this.$el.find('li').get(3).innerHTML,
-            this.$el.find('li').get(4).innerHTML,
-            this.$el.find('li').get(5).innerHTML,
-            this.$el.find('li').get(6).innerHTML,
-            this.$el.find('li').get(7).innerHTML,
-            this.$el.find('li').get(8).innerHTML
-        ];
-    },
-
-    checkWin: function() {
-        this.getValues();
-
-        // check rows
-        if (map[0] == 'X' && map[1] == 'X' && map[2] == 'X') {
-            alert('there is a winner');
-        } else if (map[3] == 'X' && map[4] == 'X' && map[5] == 'X') {
-            alert('there is a winner');
-        } else if (map[6] == 'X' && map[7] == 'X' && map[8] == 'X') {
-            alert('there is a winner');
-        }
-
-        // check cols
-        if (map[0] == 'X' && map[3] == 'X' && map[6] == 'X') {
-            alert('there is a winner');
-        } else if (map[1] == 'X' && map[4] == 'X' && map[7] == 'X') {
-            alert('there is a winner');
-        } else if (map[2] == 'X' && map[5] == 'X' && map[8] == 'X') {
-            alert('there is a winner');
-        }
-
-        // check diagonals
-        if (map[0] == 'X' && map[4] == 'X' && map[8] == 'X') {
-            alert('there is a winner');
-        } else if (map[2] == 'X' && map[4] == 'X' && map[6] == 'X') {
-            alert('there is a winner');
-        }
-    },
-
-    setPlayer: function(e) {
-        if (player === 1) {
-            player --;
-            this.setXPlayer(e);
-            this.checkWinner();
-        } else {
-            player ++;
-            this.setOPlayer(e);
-            this.checkWinner();
-        }
-    },
-
-    setXPlayer: function(e) {
-        e.currentTarget.innerHTML = 'X';
-    },
-
-    setOPlayer: function(e) {
-        e.currentTarget.innerHTML = '0';
+        'click li'       : 'isEmpty',
+        'click .play'    : 'hideModalWinner',
+        'click .restart' : 'restartGame'
     },
 
     isEmpty: function(e) {
         if (e.currentTarget.innerHTML) {
             return;
-        };
+        }
 
         this.setPlayer(e);
+    },
+
+    setPlayer: function(e) {
+        if (this.counter === 1) {
+            this.counter --;
+            this.setPlayerX(e);
+            this.checkWinner(this.playerX);
+        } else {
+            this.counter ++;
+            this.setPlayerO(e);
+            this.checkWinner(this.playerO);
+        }
+    },
+
+    setPlayerX: function(e) {
+        e.currentTarget.innerHTML = this.playerX;
+    },
+
+    setPlayerO: function(e) {
+        e.currentTarget.innerHTML = this.playerO;
+    },
+
+    restartGame: function() {
+        this.counter = 1;
+        this.$el.find('li').html('');
+    },
+
+    getValues: function() {
+        el  = this.$el.find('li');
+        map = [
+            el.get(0).innerHTML, el.get(1).innerHTML, el.get(2).innerHTML,
+            el.get(3).innerHTML, el.get(4).innerHTML, el.get(5).innerHTML,
+            el.get(6).innerHTML, el.get(7).innerHTML, el.get(8).innerHTML
+        ];
+        match = [
+            [0,1,2],[3,4,5],[6,7,8],[0,3,6],
+            [1,4,7],[2,5,8],[0,4,8],[2,4,6]
+        ];
+    },
+
+    checkWinner: function(player) {
+        this.getValues();
+
+        for (var a = 0; a < match.length; a++) {
+            if (map[match[a][0]] == player && map[match[a][1]] == player && map[match[a][2]] == player) {
+                this.showModalWinner(player);
+            }
+        }
+    },
+
+    showModalWinner: function(player) {
+        this.$el.find('.success b').text(player);
+        this.$el.find('.hide').removeClass('hide');
+    },
+
+    hideModalWinner: function() {
+        this.$el.find('.overlay').addClass('hide');
+        this.$el.find('.success').addClass('hide');
+
+        this.restartGame();
     }
 });
