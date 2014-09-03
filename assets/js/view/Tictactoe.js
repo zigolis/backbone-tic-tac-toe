34e29c2e37bc747c17be2f7757fbb8c256161134
Fernando Jorge Mota <fjorgemota@meritt.com.br>
2014-09-03 10:32:46 -0300

    Avoid creating global variables 'match' and 'map'

--- assets/js/view/Tictactoe.js
+++ assets/js/view/Tictactoe.js
@@ -4,7 +4,9 @@ app.TictactoeView = Backbone.View.extend({
     counter: 1,
     playerX: 'X',
     playerO: '0',
-
+    map: [],
+    match: [],
+    
     el: '.stage',
 
     events: {
@@ -48,12 +50,12 @@ app.TictactoeView = Backbone.View.extend({
 
     getValues: function() {
         el  = this.$el.find('li');
-        map = [
+        this.map = [
             el.get(0).innerHTML, el.get(1).innerHTML, el.get(2).innerHTML,
             el.get(3).innerHTML, el.get(4).innerHTML, el.get(5).innerHTML,
             el.get(6).innerHTML, el.get(7).innerHTML, el.get(8).innerHTML
         ];
-        match = [
+        this.match = [
             [0,1,2],[3,4,5],[6,7,8],[0,3,6],
             [1,4,7],[2,5,8],[0,4,8],[2,4,6]
         ];
@@ -62,8 +64,8 @@ app.TictactoeView = Backbone.View.extend({
     checkWinner: function(player) {
         this.getValues();
 
-        for (var a = 0; a < match.length; a++) {
-            if (map[match[a][0]] == player && map[match[a][1]] == player && map[match[a][2]] == player) {
+        for (var a = 0; a < this.match.length; a++) {
+            if (this.map[this.match[a][0]] == player && this.map[this.match[a][1]] == player && this.map[this.match[a][2]] == player) {
                 this.showModalWinner(player);
             }
         }
